b8847646f82749aa048ae42873d0380cc1d54967
aldyahsn <aldy.ahsandin@gmail.com>
2023-01-10 08:18:44 +0700

    list display admin

--- src/ks_blog/admin.py
+++ src/ks_blog/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+
 @admin.register(Post)
 class AdminPost(admin.ModelAdmin):
+    list_display = ('title', 'created', 'modified', 'author', 'status')
     fieldsets = [
-        (None, { 'fields': [('title','body', 'published', 'status')] } ),
+        (None, {'fields': [('title', 'body', )]}),
+        ('Properties', {'fields': [('published', 'status')]}),
     ]
 
     def save_model(self, request, obj, form, change):
